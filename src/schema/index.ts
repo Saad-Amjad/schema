30a2b135b12665d5babe124f5fb1d4ee6f05920c
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2019-11-17 14:38:58 +0600

    project init with core and token schemas

---
+++ src/schema/index.ts
@@ -0,0 +1,35 @@
+import {
+  apply,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+
+export function token(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const sourceTemplates = url('./files/core/src');
+    const sourceParametrizedTemplates = apply(
+      sourceTemplates, [template({
+        ..._options,
+        ...strings,
+      })]);
+    const rule = mergeWith(sourceParametrizedTemplates);
+    return rule(tree, _context);
+  };
+}
+export function core(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const sourceTemplates = url('./files/token/src');
+    const sourceParametrizedTemplates = apply(
+      sourceTemplates, [template({
+        ..._options,
+        ...strings,
+      })]);
+    const rule = mergeWith(sourceParametrizedTemplates);
+    return rule(tree, _context);
+  };
+}
